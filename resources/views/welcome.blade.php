1eefc7abdc1288c1b32b274ed12c82e3327d5412
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-19 22:39:22 +0100

    Ajout de fichiers de configuration, de routes et de tests pour initialiser le projet Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Azer</title>
+</head>
+<body>
+    <h1>Welcome to Azer Network</h1>
+    <p>Click the button bellow to view the list of Azers.</p>
+
+    <a href="" class="btn">
+        Find Azers!
+    </a>
+</body>
+</html>
