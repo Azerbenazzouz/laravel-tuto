428338e73a4524a96c4071eb0572286f8558b611
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-19 22:50:42 +0100

    Ajout de la route pour afficher la page des Azers et création de la vue associée

---
+++ resources/views/azers/index.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ninja Network</title>
+</head>
+<body>
+  <h2>Currently Available Azers</h2>
+  
+  <ul>
+    <li>Azers here</li>
+  </ul>
+  
+</body>
+</html>
